23b2b8fc3b65edaa74ec8f69612f70934caa368d
Dave Kaplan <workalicious@gmail.com>
2013-10-22 09:43:00 -0500

    added 2 routes, one for github the other just to test.

--- app.rb
+++ app.rb
@@ -2,5 +2,15 @@ class App < Sinatra::Base
   get '/' do
     'hello, world yo!'
   end
+
+  get '/something' do
+    'something here?'
+  end
+
+  post '/github-update' do
+    puts 'updating?'
+  end
+
   run! if app_file == $0
+
 end
